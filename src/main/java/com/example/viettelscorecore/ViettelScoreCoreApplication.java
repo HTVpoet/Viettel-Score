d1cf43dcde68e93e26934b043099afda0b23b541
haivt <truonghai.hp2003@gmail.com>
2026-05-28 23:51:56 +0700

    First commit

---
+++ src/main/java/com/example/viettelscorecore/ViettelScoreCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.viettelscorecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViettelScoreCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViettelScoreCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/viettelscorecore/ViettelScoreCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viettelscorecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViettelScoreCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
